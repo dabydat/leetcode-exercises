27baea8fdfd43110a03b44a43ee78d50f8856608
dabydat <davidgomezdev@gmail.com>
2025-11-07 12:36:13 -0400

    feat: added solution for 2666. Allow One Function Call problem

---
+++ Difficult[EASY]/2666. Allow One Function Call/solution.ts
@@ -0,0 +1,20 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let state: boolean = false;
+    return function (...args) {
+        let value = undefined;
+        if (!state) value = fn(...args)
+        state = true;
+        return value
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
